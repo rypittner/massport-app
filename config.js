208551d2776dd2b729034e03f707423f329de74f
rypittner <103952977+rypittner@users.noreply.github.com>
2026-01-09 14:05:13 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+const CONFIG = {
+    tsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSz_zdXMghmyVYL5ugKVPciQ2f9o7vJEPokp1YKS1lZ0_SamkfF40hQlSMYdPhvmlCTlf2ZWojFlo2p/pub?gid=0&single=true&output=tsv',
+    themes: ['#4478A2','#C65265','#D78143','#297381','#89974F','#DAAD60','#757498','#AA7876','#D7A995','#6E917A'],
+    inkPalette: ['#1B4F72', '#78281F', '#186A3B', '#7E5109', '#512E5F', '#117864'],
+    abbrToState: { 
+        "AL": "ALABAMA", "AK": "ALASKA", "AZ": "ARIZONA", "AR": "ARKANSAS", "CA": "CALIFORNIA", 
+        "CO": "COLORADO", "CT": "CONNECTICUT", "DE": "DELAWARE", "FL": "FLORIDA", "GA": "GEORGIA", 
+        "HI": "HAWAII", "ID": "IDAHO", "IL": "ILLINOIS", "IN": "INDIANA", "IA": "IOWA", 
+        "KS": "KANSAS", "KY": "KENTUCKY", "LA": "LOUISIANA", "ME": "MAINE", "MD": "MARYLAND", 
+        "MA": "MASSACHUSETTS", "MI": "MICHIGAN", "MN": "MINNESOTA", "MS": "MISSISSIPPI", 
+        "MO": "MISSOURI", "MT": "MONTANA", "NE": "NEBRASKA", "NV": "NEVADA", "NH": "NEW HAMPSHIRE", 
+        "NJ": "NEW JERSEY", "NM": "NEW MEXICO", "NY": "NEW YORK", "NC": "NORTH CAROLINA", 
+        "ND": "NORTH DAKOTA", "OH": "OHIO", "OK": "OKLAHOMA", "OR": "OREGON", "PA": "PENNSYLVANIA", 
+        "RI": "RHODE ISLAND", "SC": "SOUTH CAROLINA", "SD": "SOUTH DAKOTA", "TN": "TENNESSEE", 
+        "TX": "TEXAS", "UT": "UTAH", "VT": "VERMONT", "VA": "VIRGINIA", "WA": "WASHINGTON", 
+        "WV": "WEST VIRGINIA", "WI": "WISCONSIN", "WY": "WYOMING", "EU": "EUROPE" 
+    },
+    stateIcons: { 
+        "AZ": "brightness_7", "CA": "beach_access", "PA": "notifications_active", "EU": "public", 
+        "NY": "apartment", "FL": "sunny", "TX": "star", "NV": "playing_cards", "WA": "coffee", 
+        "HI": "surfing", "CO": "mountain_flag", "MA": "history_edu", "IL": "location_city", 
+        "LA": "music_note", "TN": "music_note", "GA": "agriculture", "OR": "forest", 
+        "ME": "sailing", "DC": "gavel", "UT": "hiking", "NM": "light_mode", "OH": "flight", 
+        "MI": "kayaking", "AK": "ac_unit", "WI": "forest", "VT": "ac_unit", "MT":"landscape", 
+        "NE":"psychiatry" 
+    },
+    defaultIcons: ['explore', 'place', 'map', 'flag', 'travel_explore']
+};
